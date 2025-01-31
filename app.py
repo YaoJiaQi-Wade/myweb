6d8f03ad5abbb7dffa4363d41f406fa0958e10a8
wade_yao <yaojiaqi1201@qq.com>
2025-01-31 22:22:59 +0800

    20250131_2222_commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS  # 解決跨域問題
 
 app = Flask(__name__)
 CORS(app)  # 允許所有域訪問（僅供開發測試用）
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/hello')
 def hello():
